f69610b17a78ab24cdea2a31858da580c0cfa035
Andrea <andrea.distler@gmail.com>
2012-11-12 12:08:02 +0100

    added sprites, drawing player

---
+++ src/Constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace GTEngine
+{
+	const float PI = 3.14159f;
+	const float DEG2RAD = PI / 180.0f;
+	const float RAD2DEG = 180.0f / PI;
+	const float GRAVITY = -5.81f;
+};
+
+#endif
